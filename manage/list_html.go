b70732c8df2cd14bb7a6e29c02d193f01d33a8ce
bughou <bughou@gmail.com>
2019-07-07 00:13:41 +0800

    update manage page

--- manage/list_html.go
+++ manage/list_html.go
@@ -16,27 +16,36 @@ input:focus { outline-width: 0; }
     </style>
   </head>
   <body>
-    <p>To see a spefic value in data, click at blank area after data link,
-    input map keys or slice indexes seperated by "," , press ENTER.
+    <p>To see a spefic value in data, click at blank area after data link to show input box(click again to hide it).
+    Input map keys or slice indexes seperated by "," and press ENTER.
     </p>
     ` + table + `
     <script>
       function createInput(event) {
         var td = event.currentTarget;
         if (td != event.target) return;
-        td.removeEventListener('click',  createInput);
 
-        var input = document.createElement('input');
-        input.addEventListener("keyup", function(event) {
-          if (event.keyCode !== 13) return;
-          input.value = input.value.trim();
-          if (input.value == "") return;
-          window.open(td.firstElementChild.getAttribute("href") + "?keys=" + input.value)
-        });
+        if (td.lastElementChild.tagName != 'INPUT') {
+          var input = document.createElement('input');
+          input.addEventListener("keyup", function(event) {
+            if (event.keyCode !== 13) return;
+            input.value = input.value.trim();
+            if (input.value == "") return;
+            window.open(td.firstElementChild.getAttribute("href") + "?keys=" + input.value)
+          });
 
-        td.appendChild(document.createElement('br'));
-        td.appendChild(input);
-        input.focus();
+          td.appendChild(document.createElement('br'));
+          td.appendChild(input);
+          input.focus();
+        } else {
+          var style = td.lastElementChild.style;
+          if (style.display === '') {
+            style.display = 'none';
+          } else {
+            style.display = '';
+            td.lastElementChild.focus();
+          }
+        }
       }
 
       (function() {
